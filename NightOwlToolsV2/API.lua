895de487e33db8b441f253fe90b766899a55a77d
v012345 <v138468@gmail.com>
2024-08-05 18:07:13 +0800

    加入 api, 给 vscode 提示

---
+++ NightOwlToolsV2/API.lua
@@ -0,0 +1,11 @@
+---@class file_state
+local file_state = {
+    id = 1,
+    path = "/a/b/c.png",
+    name = "c.png",
+    modification = 1,
+    sha1 = "abcdef",
+    relative_path = "b/c.png",
+    key = "b/c.png",
+    need_update = true
+}
