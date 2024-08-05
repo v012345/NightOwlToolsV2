2624ba25b6c7e1e51a18757f3fefbd8f3a501da2
v012345 <v138468@gmail.com>
2024-08-05 10:25:06 +0800

    更新 Log

--- NightOwlToolsV2/Tools/Log.lua
+++ NightOwlToolsV2/Tools/Log.lua
@@ -3,7 +3,7 @@ Log = {}
 setmetatable(Log, {
     __call = function(self, str)
         if not self.logFile then
-            self.logFile = io.open("Log.txt", "w")
+            self.logFile = io.open("LocalOnly/Log.txt", "w")
         end
         self.logFile:write(str)
         return self
