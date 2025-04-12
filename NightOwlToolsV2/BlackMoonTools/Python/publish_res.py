d4d54425d7401ba18f945c57791afba1d2c3be6e
v138468 <v012345@163.com>
2025-04-12 11:02:14 +0800

    美化输出

--- NightOwlToolsV2/BlackMoonTools/Python/publish_res.py
+++ NightOwlToolsV2/BlackMoonTools/Python/publish_res.py
@@ -83,15 +83,12 @@ if __name__ == "__main__":
     print("调用 lua 获取需要更新资源")
     fDealFiles = output_dir+"/deal_files.txt"
     subprocess.Popen([lua_exe, 'lua\\getNeedDealFiles.lua', origin_dir, output_dir, fDealFiles],
-                     creationflags=subprocess.CREATE_NEW_CONSOLE).wait()
+                     creationflags=subprocess.CREATE_NEW_CONSOLE
+                     ).wait()
     with open(fDealFiles, 'r', encoding='utf-8') as f:
         need_deal = [line.strip()
                      for line in f if line.strip()]  # 去掉空行和换行
-        
-    # 完成之后 更新 lua 的文件状态数据库
-    subprocess.Popen([lua_exe, 'lua\\updateFileStateDB.lua', origin_dir, output_dir, fDealFiles],
-                     creationflags=subprocess.CREATE_NEW_CONSOLE).wait()
-    exit(0)
+
     # 过滤出要预处理图集资源
     need_preprocess = [
         p for p in need_deal
@@ -102,28 +99,29 @@ if __name__ == "__main__":
     common = list(set(need_deal) - set(need_preprocess))
 
     # 加密文件
-    i, tatol = 1, len(common)
+    i, tatol = 0, len(common)
+    print(f"加密共有资源 : {i}/{tatol}", end='\r')
     if tatol > 0:
         for f in common:
             encrypt_file(origin_dir+f, output_dir+"/encrypted_common"+f)
-            print(f"加密共有资源 : {i}/{tatol}", end='\r')
             i += 1
-    else:
-        print(f"加密共有资源 : 0/0", end='\r')
+            print(f"加密共有资源 : {i}/{tatol}", end='\r')
     print()
 
-    i, tatol = 1, len(common)
+    i, tatol = 0, len(common)
+    print(f"压缩共有的已加密资源 : {i}/{tatol}", end='\r')
     for f in common:
         with open(output_dir+"/encrypted_common" + f, 'rb') as t:
             c = t.read()
         with gzip.GzipFile(filename='', mode='wb', compresslevel=9, mtime=1, fileobj=open(output_dir+"/compressed_common" + f + ".gz", 'wb')) as g:
             g.write(c)
-        print(f"压缩共有的已加密资源 : {i}/{tatol}", end='\r')
         i += 1
+        print(f"压缩共有的已加密资源 : {i}/{tatol}", end='\r')
     print()
 
     common_list_v1, common_list_v2 = {}, {}
-    i, tatol = 1, len(common)
+    i, tatol = 0, len(common)
+    print(f"计算共有文件签名 : {i}/{tatol}", end='\r')
     for f in common:
         encrypt_res_md5 = md5(output_dir+"/encrypted_common"+f)
         gz_file_name = output_dir+"/compressed_common"+f+".gz"
@@ -132,8 +130,8 @@ if __name__ == "__main__":
         size = os.path.getsize(gz_file_name)
         common_list_v1[key] = [encrypt_res_md5, size, key]
         common_list_v2[key] = [encrypt_res_md5, size, key, compressed_res_md5]
-        print(f"计算共有文件签名 : {i}/{tatol}", end='\r')
         i += 1
+        print(f"计算共有文件签名 : {i}/{tatol}", end='\r')
     print()
 
     # 打开文件写入
@@ -147,37 +145,39 @@ if __name__ == "__main__":
         preprocessed_files = output_dir + "/"+p+"_preprocessed_files.txt"
         os.system(preprocess_cmd[p] % (
             origin_dir, output_dir+"/preprocessed_"+p, preprocess_files, preprocessed_files))
-        
+
         # 加密处理出来的文件
         with open(preprocessed_files, 'r', encoding='utf-8') as f:
             dealed_files = [line.strip() for line in f if line.strip()]
-        i, tatol = 1, len(dealed_files)
+
+        i, tatol = 0, len(dealed_files)
+        print(f"加密 {p} 独有资源 : {i}/{tatol}", end='\r')
         for f in dealed_files:
             encrypt_file(output_dir+"/preprocessed_"+p +
                          f, output_dir+"/encrypted_"+p+f)
-            print(f"加密 {p} 独有资源 : {i}/{tatol}", end='\r')
             i += 1
+            print(f"加密 {p} 独有资源 : {i}/{tatol}", end='\r')
         print()
 
         # 压缩加密的文件
-        i, tatol = 1, len(dealed_files)
+        i, tatol = 0, len(dealed_files)
+        print(f"压缩 {p} 独有的已加密资源 : {i}/{tatol}", end='\r')
         for f in dealed_files:
             with open(output_dir+"/encrypted_"+p+f, 'rb') as t:
                 c = t.read()
             with gzip.GzipFile(filename='', mode='wb', compresslevel=9, mtime=1, fileobj=open(output_dir+"/compressed_"+p+f + ".gz", 'wb')) as g:
                 g.write(c)
-            print(f"压缩 {p} 独有的已加密资源 : {i}/{tatol}", end='\r')
             i += 1
+            print(f"压缩 {p} 独有的已加密资源 : {i}/{tatol}", end='\r')
         print()
-
 
         # 生成对比文件, v1 和 v2 只是热更程序升级了 v2 版, 但是为了兼容 v1 版, 还要生成 v1 的 list
         list_v1, list_v2 = {}, {}
         read_list(output_dir+"/encrypted_"+p+"/list.csv", list_v1)
         read_list(output_dir+"/encrypted_"+p+"/list-v2.csv", list_v2)
 
-
-        i, tatol = 1, len(dealed_files)
+        i, tatol = 0, len(dealed_files)
+        print(f"计算 {p} 资源的签名 : {i}/{tatol}", end='\r')
         for f in dealed_files:
             encrypt_res_md5 = md5(output_dir+"/encrypted_"+p+f)
             gz_file_name = output_dir+"/compressed_"+p+f+".gz"
@@ -186,9 +186,10 @@ if __name__ == "__main__":
             size = os.path.getsize(gz_file_name)
             list_v1[key] = [encrypt_res_md5, size, key]
             list_v2[key] = [encrypt_res_md5, size, key, compressed_res_md5]
-            print(f"计算 {p} 资源的签名 : {i}/{tatol}", end='\r')
             i += 1
+            print(f"计算 {p} 资源的签名 : {i}/{tatol}", end='\r')
         print()
+
         for i in [{"data": list_v1 | common_list_v1, "path": "/list.csv"}, {"data": list_v2 | common_list_v2, "path": "/list-v2.csv"}]:
             data = i["data"]
             with open(output_dir+"/encrypted_"+p + i["path"], 'w', newline='', encoding='utf-8') as f:
@@ -199,7 +200,6 @@ if __name__ == "__main__":
                 c = f.read()
             with gzip.GzipFile(filename='', mode='wb', compresslevel=9, mtime=1, fileobj=open(output_dir+"/compressed_"+p + i["path"] + ".gz", 'wb')) as g:
                 g.write(c)
-    print('lua\\updateFileStateDB.lua', origin_dir, output_dir, fDealFiles)
     # 完成之后 更新 lua 的文件状态数据库
     subprocess.Popen([lua_exe, 'lua\\updateFileStateDB.lua', origin_dir, output_dir, fDealFiles],
                      creationflags=subprocess.CREATE_NEW_CONSOLE).wait()
