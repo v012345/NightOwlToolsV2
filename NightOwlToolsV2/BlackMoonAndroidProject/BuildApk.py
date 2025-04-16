64d4f49701dc6c2832d3683295705090e2a72f8b
v138468 <v012345@163.com>
2025-04-16 10:34:08 +0800

    删除 黑月 安卓工程

--- NightOwlToolsV2/BlackMoonAndroidProject/BuildApk.py
+++
@@ -1,7 +0,0 @@
-import os
-if __name__ == '__main__':
-    project_path = os.path.abspath( "application" )
-    project_name = os.path.basename( project_path )
-    build_xml = os.path.join( project_path, "build.xml" )
-    os.system( 'C:\\work\\tools\\android_sdk\\tools\\android.bat update project --target 2 --path C:\\work\\BlackMoon\\Closers.android\\application --name application' )
-    
--- NightOwlToolsV2/BlackMoonAndroidProject/_uusdk/libs/bugly/BuglySDK/Android/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := bugly_native_prebuilt
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libBugly.so
-
-include $(PREBUILT_SHARED_LIBRARY)
