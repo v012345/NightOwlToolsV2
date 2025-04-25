483ec5628faf1b729a53e956a79dd23bd54f0233
v138468 <v012345@163.com>
2025-04-25 23:26:32 +0800

    加入新工具

---
+++ NightOwlToolsV2/BlackMoonTools/lua/getFilesOfDirWithExt.lua
@@ -0,0 +1,35 @@
+require "lua.Common"
+
+local excludeDir = { -- .git, .svn 之类, 不去比较
+    [".git"] = true,
+    [".svn"] = true,
+    [".vscode"] = true
+}
+local ext = {
+    [".c"] = true,
+    [".cpp"] = true,
+    [".cc"] = true,
+}
+local function get_extension(filename)
+    return filename:match("^.+(%..+)$")
+end
+local from = arg[1]
+local output_to = arg[2]
+local x = {}
+os.execute("chcp 65001 >nul")
+print("正在获取")
+print(from)
+print("下的全部文件")
+Common.GetAllFilesOfDirectory(from, excludeDir, x)
+local total = #x
+local y = {}
+for i, v in ipairs(x) do
+    io.write(string.format("%s/%s\r",i,total))
+    if ext[get_extension(v)] then
+        y[#y + 1] = string.gsub(v, from, "", 1)
+    end
+end
+
+local file = io.open(output_to, "w") or error("can't open " .. output_to)
+file:write(table.concat(y, "\n"))
+file:close()
