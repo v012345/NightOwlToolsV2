c71bed7b0d069df73692077d05e80fa735f58a4d
v138468 <v012345@163.com>
2025-05-06 23:07:46 +0800

    加入新工具

---
+++ NightOwlToolsV2/BlackMoonTools/local/把工程文件复制到这里.cmd
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+cls
+
+@echo off
+pushd "%~dp0"
+cd ..
+..\NightOwlToolsV2.exe lua\copyDirStruct.lua "C:\work\BlackMoon\BlackMoonAndroid\engine" "engine" 
+..\NightOwlToolsV2.exe lua\copyEngineFiles.lua "C:\work\BlackMoon\BlackMoonAndroid\engine" "engine" "temp\engine_files.txt"
+..\NightOwlToolsV2.exe lua\remove_empty_dirs.lua "engine"
+popd
+
+pause
