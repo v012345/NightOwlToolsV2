6500816a8b0e99cb5a1685b2078a1657c1a03a83
v138468 <v012345@163.com>
2025-05-06 16:21:24 +0800

    加入新工具

---
+++ NightOwlToolsV2/BlackMoonTools/local/同步主干到 engine.cmd
@@ -0,0 +1,22 @@
+@echo off
+chcp 65001 >nul
+cls
+
+@echo off
+pushd "%~dp0"
+cd ..
+..\NightOwlToolsV2.exe lua\getFilesOfDir.lua "engine\src\main\cpp\cocos2d" "temp\engine_cocos2d.txt"
+..\NightOwlToolsV2.exe lua\copyFileByTxt.lua "C:\work\BlackMoon\Closers.cocos\client\trunk\cocos2d" "engine\src\main\cpp\cocos2d" "temp\engine_cocos2d.txt"
+
+..\NightOwlToolsV2.exe lua\getFilesOfDir.lua "engine\src\main\cpp\Classes" "temp\engine_Classes.txt"
+..\NightOwlToolsV2.exe lua\copyFileByTxt.lua "C:\work\BlackMoon\Closers.cocos\client\trunk\Classes" "engine\src\main\cpp\Classes" "temp\engine_Classes.txt"
+
+@REM 这个我也不知道是在哪里拿过来的, 太乱了
+..\NightOwlToolsV2.exe lua\getFilesOfDir.lua "engine\src\main\cpp\aonesdkc" "temp\engine_aonesdkc.txt"
+..\NightOwlToolsV2.exe lua\copyFileByTxt.lua "C:\work\BlackMoon\Closers.android-studio\Project\aonesdkc" "engine\src\main\cpp\aonesdkc" "temp\engine_aonesdkc.txt"
+
+..\NightOwlToolsV2.exe lua\getFilesOfDir.lua "engine\src\main\cpp\closers" "temp\engine_closers.txt"
+..\NightOwlToolsV2.exe lua\copyFileByTxt.lua "C:\work\BlackMoon\Closers.cocos\client\trunk\proj.android\jni\closerscpp" "engine\src\main\cpp\closers" "temp\engine_closers.txt"
+popd
+
+pause
