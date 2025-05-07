cc225c36e5d1abea557b10bcc4fb515da202f74e
v138468 <v012345@163.com>
2025-05-07 22:58:35 +0800

    删除无用文件

--- NightOwlToolsV2/CopyDirStructure/CopyDirStructure.lua
+++
@@ -1,6 +0,0 @@
-require "CopyDirStructure.config"
-local exclude = {}
-for i, v in ipairs(CopyDirStructure.Exclude) do
-    exclude[v] = i
-end
-CopyDirStructure.copyDirStructure(CopyDirStructure.Source, CopyDirStructure.Target, exclude)
--- NightOwlToolsV2/CopyDirStructure/config.lua
+++
@@ -1,4 +0,0 @@
-CopyDirStructure = CopyDirStructure or {}
-CopyDirStructure.Source = "D:/20240731_1_2_0_launch/13_Execution/unity3dProj/Project/Assets/Download/xLuaLogic"
-CopyDirStructure.Target = "D:/20240731_1_2_0_launch/13_Execution/unity3dProj/Project/Assets/DownloadVI/xLuaLogic"
-CopyDirStructure.Exclude = { ".svn", ".git" }
