79477a1f0fb36dc628ee2e8290a1ca2e646f59f9
v138468 <v012345@163.com>
2025-05-07 23:02:39 +0800

    改结构

--- NightOwlToolsV2/PublishRes/config.lua
+++ NightOwlToolsV2/PublishRes/config.lua
@@ -1,23 +1,13 @@
-PublishRes = {
-    CocosTool = "C:\\Cocos\\Cocos Studio\\Cocos.Tool.exe",
+Translation = {
+    ConvertorUrl = "https://github.com/v012345/NightOwlToolsV2CS/releases/download/Tool-TableConvertor/TableConvertor.zip",
     Projects = {
     -- {
     --     source = "C:/work/Closers.cocos/resource/ui/branches/qooapp/zhtw",
-    --     target = "C:/work/Closers.cocos/client/branches/qooapp/Resources/res_zhtw"
-    -- } ,
-    -- {
-    --     source = "C:/work/Closers.cocos/resource/ui/branches/handygame/ko",
-    --     target = "C:/work/Closers.cocos/client/branches/handygame/Resources/res_ko"
-    -- },
+    --     target = "C:/work/Closers.cocos/resource/ui/branches/juefeng_sea/en",
+    -- }, 
     {
-        source = "C:/work/Closers.cocos/resource/ui/branches/handygame_test/ko",
-        target = "C:/work/Closers.cocos/client/branches/handygame_test/Resources/res_ko"
-    },
-    -- {
-    --     source = "C:/work/Closers.cocos/resource/ui/branches/juefeng_sea/en",
-    --     target = "C:/work/Closers.cocos/client/branches/juefeng_sea/Resources/res_en"
-    -- }
-    },
-    DBPath = "LocalOnly/PublishRes.db",
-    TableName = "file_states"
+        source = "C:/work/Closers.cocos/resource/ui/branches/handygame_test/ko1",
+        target = "C:/work/Closers.cocos/resource/ui/branches/handygame_test/ko1"
+    }
+}
 }
