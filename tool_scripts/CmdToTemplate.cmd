f6890ac354927ce49f9ad232067377a86be770ff
v138468 <v012345@163.com>
2025-05-19 09:59:21 +0800

    工具更新

--- tool_scripts/CmdToTemplate.cmd
+++ tool_scripts/CmdToTemplate.cmd
@@ -5,5 +5,6 @@ cls
 setlocal EnableDelayedExpansion
 if exist "cmd\传奇-上传数字资源.cmd" copy /Y "cmd\传奇-上传数字资源.cmd" "传奇\上传数字资源.cmd.template"
 if exist "cmd\传奇-更新数字资源.cmd" copy /Y "cmd\传奇-更新数字资源.cmd" "传奇\更新数字资源.cmd.template"
-if exist "cmd\通用-005-删除指定类型的文件.cmd" copy /Y "cmd\通用-005-删除指定类型的文件.cmd" "通用\通用-005-删除指定类型的文件.cmd.template"
+if exist "cmd\通用-删除指定类型的文件.cmd" copy /Y "cmd\通用-删除指定类型的文件.cmd" "通用\删除指定类型的文件.cmd.template"
+if exist "cmd\通用-获取文件夹下文件的大小.cmd" copy /Y "cmd\通用-获取文件夹下文件的大小.cmd" "通用\获取文件夹下文件的大小.cmd.template"
 endlocal
--- tool_scripts/TemplateToCmd.cmd
+++ tool_scripts/TemplateToCmd.cmd
@@ -6,6 +6,7 @@ setlocal EnableDelayedExpansion
 
 copy /Y "传奇\上传数字资源.cmd.template"  "cmd\传奇-上传数字资源.cmd"
 copy /Y "传奇\更新数字资源.cmd.template"  "cmd\传奇-更新数字资源.cmd"
-copy /Y "通用\通用-005-删除指定类型的文件.cmd.template"  "cmd\通用-005-删除指定类型的文件.cmd"
+copy /Y "通用\删除指定类型的文件.cmd.template"  "cmd\通用-删除指定类型的文件.cmd"
+copy /Y "通用\获取文件夹下文件的大小.cmd.template"  "cmd\通用-获取文件夹下文件的大小.cmd"
 
 endlocal
