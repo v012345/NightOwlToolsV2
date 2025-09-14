3f844b5f75decdc259aae3c7723b21ec75adc685
v138468 <v138468@gmail.com>
2025-09-15 04:49:15 +0900

    更新表

--- tool_scripts/单词游戏/图片识别文字.cmd
+++ tool_scripts/单词游戏/图片识别文字.cmd
@@ -7,7 +7,12 @@ cd..
 
 setlocal EnableDelayedExpansion
 
-py -3 Python\imageToTxt.py --image temp\page_230.png --text temp\page_230.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_232.png --text temp\page_232.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_234.png --text temp\page_234.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_236.png --text temp\page_236.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_238.png --text temp\page_238.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_240.png --text temp\page_240.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
+py -3 Python\imageToTxt.py --image temp\page_242.png --text temp\page_242.txt --tesseract "C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 endlocal
