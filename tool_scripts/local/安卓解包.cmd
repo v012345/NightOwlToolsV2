11a21606870ccc2fb1347697ee2c8d4540a95137
v138468 <v138468@gmail.com>
2025-09-26 14:07:37 +0900

    更新打表工具

--- tool_scripts/local/安卓解包.cmd
+++ tool_scripts/local/安卓解包.cmd
@@ -5,7 +5,7 @@ cls
 @echo off
 pushd "%~dp0"
 cd ..
-java -jar jar\apktool_2.11.1.jar d temp\base.apk -f -o temp\app_src
+C:\Users\MH\.jdks\corretto-1.8.0_462\bin\java.exe -jar jar\apktool_2.11.1.jar d temp\v1.0.0_release_yougubt4_res-res_zhcn_smartspace_202509180047_yougu_andbt4_2509180526.apk -f -o temp\app_src
 popd
 pause
 
--- tool_scripts/单词游戏/生成单词图片.cmd
+++ tool_scripts/单词游戏/生成单词图片.cmd
@@ -7,7 +7,7 @@ cd..
 
 setlocal EnableDelayedExpansion
 @REM ="res://assets/words/jp_word_" & A490 & ".png"
-py -3 Python\genWordImgByCsv.py --assets_path "C:\NightOwlZone\Client\7city"
+@REM py -3 Python\genWordImgByCsv.py --assets_path "C:\NightOwlZone\Client\7city"
 py -3 Python\csvToGodotDB.py --csv "单词游戏\words.csv" --gd "C:\NightOwlZone\Client\7city\scripts\db\words\japanese.gd"
 
 
