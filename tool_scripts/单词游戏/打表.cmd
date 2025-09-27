1feb0c5ecd75380ab17ed92fa33b0baf3fd0e90f
v138468 <v138468@gmail.com>
2025-09-27 22:56:42 +0900

    加资源

--- tool_scripts/单词游戏/打表.cmd
+++ tool_scripts/单词游戏/打表.cmd
@@ -7,7 +7,7 @@ cd..
 
 setlocal EnableDelayedExpansion
 
-py -3 Python\csvToGodotDB.py --csv "单词游戏\words.csv" --gd "C:\NightOwlZone\Client\7city\scripts\db\words\japanese.gd"
+py -3 Python\csvToGodotDB.py --csv "单词游戏\japanese.csv" --gd "C:\NightOwlZone\Client\7city\scripts\db\words\japanese.gd"
 py -3 Python\csvToGodotDB.py --csv "单词游戏\example.csv" --gd "C:\NightOwlZone\Client\7city\scripts\db\words\example.gd"
 
 
