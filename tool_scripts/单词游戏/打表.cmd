03cb2c71ba118528e2de72e2e9fbeafd72383323
v138468 <v138468@gmail.com>
2025-10-13 18:38:01 +0900

    加入新单词

--- tool_scripts/单词游戏/打表.cmd
+++ tool_scripts/单词游戏/打表.cmd
@@ -9,6 +9,7 @@ setlocal EnableDelayedExpansion
 
 py -3 Python\csvToGodotDB.py --csv "单词游戏\japanese.csv" --gd "C:\NightOwlZone\Client\jlpt_pass\scripts\db\words\japanese.gd"
 py -3 Python\csvToGodotDB.py --csv "单词游戏\example.csv" --gd "C:\NightOwlZone\Client\jlpt_pass\scripts\db\words\example.gd"
+py -3 Python\csvToGodotDB.py --csv "单词游戏\books.csv" --gd "C:\NightOwlZone\Client\jlpt_pass\scripts\db\words\books.gd"
 
 
 endlocal
